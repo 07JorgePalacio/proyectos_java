07308790c65c767d01b8967f88b56fb8504d5408
Jorge Ismael Palacio Caceres <ismael.jpci@gmail.com>
2025-05-17 19:54:08 -0500

    Cambio 17/05/2025 prueba 2

--- mi_primer_proyecto_java/src/App.java
+++ mi_primer_proyecto_java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Comprobación de cambios GIT");
+        System.out.println("Comprobación de cambios GIT, otro cambio de prueba");
     }
 }
